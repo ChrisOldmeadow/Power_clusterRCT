67f21e7b7b74bb2a12830342f6077755fdb33d33
Chris Oldmeadow <chris.oldmeadow@mailbox.org>
2021-07-23 19:01:10 +1000

    added mixed model for ordinal data

---
+++ sim_clusterRCT_ordinal.R
@@ -0,0 +1,67 @@
+
+pacman::p_load(plyr, ordinal, pbapply, assertthat)
+
+
+# for a logistic or ordinal regression, the level 1 variance = p1^2/3
+# See Agresti A. Categorical data analysis. 2. Hoboken, NJ: Wiley; 2002.
+
+
+
+# The ordinal model is parameterised by cumulative response probabilities:
+
+
+
+sim_cRCT <- function(k1, # number of clusters in ctr
+                     k2, # numbner of clusters in the int
+                     m, # average cluster size
+                     p, # vector of control probabilities of each outcome level
+                     or, # intervention effect as an odds ratio
+                     rho) { # intra-class correlation
+
+  assertthat::assert_that(sum(p) == 1)
+  # random effects
+  sigma2 <- (pi^2) / 3
+  theta <- sqrt((rho * sigma2) / (1 - rho)) # TODO check this
+
+  x <- rep(c(0, 1), times = c(k1 * m, k2 * m))
+  site <- c(rep(1:(k1 + k2), each = m))
+  a <- rnorm((k1 + k2), mean = 0, theta) # site level random effect
+  arep <- rep(a, each = m)
+
+  # for 3 categories we have 2 intercepts
+  beta0 <- log((p[2] + p[3]) / p[1])
+  beta2 <- log(p[3] / (p[1] + p[2]))
+  beta1 <- log(or)
+  lp1 <- beta0 + beta1 * x + arep
+  lp2 <- beta2 + beta1 * x + arep
+  p1 <- 1 - (exp(lp1) / (1 + exp(lp1)))
+  p3 <- exp(lp2) / (1 + exp(lp2))
+  p2 <- (exp(lp1) / (1 + exp(lp1))) - p3
+  probs <- cbind(p1, p2, p3)
+  y <- apply(probs, 1, function(x) sample(1:3, 1, prob = x))
+  fit <- clmm(factor(y) ~ x + (1 | site)) # nolint
+  p <- summary(fit)$coefficients[3, 4] # nolint
+  # return(data.frame(y, x, site, probs))
+  return(p)
+}
+
+
+# now the power calculations
+nsims <- 1000
+fit_all <- pbreplicate(
+  nsims,
+  sim_cRCT(
+    k1 = 6, k2 = 6, m = 38,
+    p = c(.3, .4, .3), or = 1.3, rho = 0.05
+  )
+)
+
+sum(is.na(fit_all)) / nsims
+mean(fit_all < 0.05, na.rm = TRUE)
+
+# test <- sim_cRCT(k1 = 6, k2 = 6, m = 38, p = c(.1, .3, .6), or = 1.5, rho = .05)
+# table(test$y[test$x == 0]) / (0.5*nrow(test))
+# table(test$y[test$x == 1]) / (0.5*nrow(test))
+#
+#
+# exp(MASS::polr( factor(y) ~ x, data = test)$coefficients)
